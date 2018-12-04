52fdeb56a13316d53314aebb3e2bb15b0010bc24
User-PC\User <sollozzo@bu.edu>
2018-12-04 01:54:40 -0500

    myhist update

--- my-histogram.c
+++ my-histogram.c
@@ -6,6 +6,45 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
+/*
+ * main takes in a directory name, passes it to mydir
+ * mydir counts the number of each type of file
+ * creates an int array and passes it to formatdat
+ * formatdat takes the int array and makes it readable for gnuplot
+ * writes it to data1.dat
+ * then it prints out the right instructions for making a hist in gnuplot
+ * using the data in data1.dat
+ * 
+ * to make it run:
+ * apt-get install gnuplot
+ * gcc my-histogram.c -o my-histogram
+ * ./my-histogram | gnuplot
+ * then there will be a gif image to send back to the client
+ * 
+ * */
+void formatdat(int data[7]) {
+	FILE * datafile;
+	char strbuf[50];
+	const char *types[] = {"Regular","Directory","FIFO","Sockets","Symlinks","CHR","BLK"};
+	
+	if ((datafile = fopen("data1.dat","w")) == NULL) {
+        printf("Error! opening file in formatdat");
+        exit(1);
+    }
+    
+    int i;
+    char numstr[10];
+    for (i = 0; i < 7; i++) {
+		strcpy(strbuf, types[i]);
+		strcat(strbuf, "    ");
+		sprintf(numstr, "%d", data[i]);
+		strcat(strbuf, numstr);
+		strcat(strbuf, "\n");
+		fwrite(strbuf, 1, strlen(strbuf), datafile);
+		strbuf[0] = '\0';
+	}
+	fclose(datafile);
+}
 
 void mydir(char * pathname) {
 	DIR *dp;
@@ -26,7 +65,7 @@ void mydir(char * pathname) {
 		strcpy(curfilestr,pathname);
 		strcat(curfilestr,"/");
 		strcat(curfilestr,curfilename);
-		printf("curfilename: %s\n",curfilename);
+		//printf("curfilename: %s\n",curfilename);
 		if (stat(curfilestr, &curfile) != 0) {
 			printf("statting error\n");
 		}
@@ -55,39 +94,15 @@ void mydir(char * pathname) {
 
 		}
 	}
-	int i;
-	for (i = 0; i < 7; i++) {
-		printf("%d ", tally[i]);
-	}
-	printf("\n");
-}
-	
-void formatdat(int data[7]) {
-	FILE * datafile;
-	char strbuf[50];
-	const char *types[] = {"Regular","Directory","FIFO","Sockets","Symlinks","CHR","BLK"};
-	
-	if ((datafile = fopen("data1.dat","w")) == NULL) {
-        printf("Error! opening file in formatdat");
-        exit(1);
-    }
-    
-    int i;
-    char numstr[10];
-    for (i = 0; i < 7; i++) {
-		strcpy(strbuf, types[i]);
-		strcat(strbuf, "    ");
-		sprintf(numstr, "%d", data[i]);
-		strcat(strbuf, numstr);
-		strcat(strbuf, "\n");
-		fwrite(strbuf, 1, strlen(strbuf), datafile);
-		strbuf[0] = '\0';
-	}
-	fclose(datafile);
+	formatdat(tally);
 }
 
 int main(int argc, char * argv[]) {
-	if (argc != 2) printf("Argument number not correct\n");
+	if (argc < 2) printf("you have to enter a directory\n");
+	if (argc > 2) printf("too many args but proceeding anyway\n");
 	mydir(argv[1]);
+	char * toprint = "reset\n\nset xlabel \"File Type\"\nset ylabel \"Number of Files\"\nset grid\nset boxwidth 0.95 relative\nset style fill transparent solid 0.5 noborder\nplot \"data1.dat\" u 2:xticlabels(1) w boxes lc rgb\"green\" notitle\n\nset yrange [GPVAL_Y_MIN-1:GPVAL_Y_MAX+2]\nset terminal gif\nset output \"myhistresult.gif\"\nreplot";
+	printf(toprint);
 	return 1;
+	//send result gif to client after this
 }
