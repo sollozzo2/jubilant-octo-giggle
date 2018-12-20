77c2dfed8b348687c4560ceb435359d2e2aff08c
User-PC\User <sollozzo@bu.edu>
2018-12-20 18:22:47 -0500

    updating server.c

--- server.c
+++ server.c
@@ -8,7 +8,7 @@
 #include <dirent.h>
 //#define PORT 6969
 char * fourofour = "HTTP/1.1 404 Not Found\r\nContent-type: text/html\r\n\r\n<html>\
-<head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><body></html>"
+<head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><body></html>";
 
 
 void list_dir(char *dir_name, char* file_str);
@@ -17,6 +17,7 @@ void htmlhandler(char * filename, int new_socket) {
     FILE * toopen;
     if ((toopen = fopen(filename,"r")) == NULL) {
         printf("Error! opening file in htmlhandle");
+    send(new_socket, fourofour, strlen(fourofour), 0);
         exit(1);
     }
     char * header = "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n";
@@ -154,60 +155,60 @@ void list_dir(char *dir_name, char* file_str) {
 
 int main(int argc, char const *argv[]) 
 { 
-	FILE * fptr;
-	pid_t pid;
-	int new_socket;
-	int server_fd, valread; 
-	struct sockaddr_in address; 
-	int opt = 1; 
-	int addrlen = sizeof(address); 
-	char buffer[1024] = {0}; 
-	char *hello = "Hello from server"; 
+    FILE * fptr;
+    pid_t pid;
+    int new_socket;
+    int server_fd, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
 
     if (argv[1] == NULL) printf("need PORT arg!!\n");
     int port = strtol(argv[1], NULL, 10);
-	
-	// Creating socket file descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-	{ 
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	
-	// Forcefully attaching socket to the port 6969
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-												&opt, sizeof(opt))) 
-	{ 
-		perror("setsockopt"); 
-		exit(EXIT_FAILURE); 
-	} 
-	address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons( port ); 
-	
-	// Forcefully attaching socket to the port 8080 
-	if (bind(server_fd, (struct sockaddr *)&address, 
-								sizeof(address))<0) 
-	{ 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	if (listen(server_fd, 3) < 0) 
-	{ 
-		perror("listen"); 
-		exit(EXIT_FAILURE); 
-	}
-	
-	while (1) {
-		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-					(socklen_t*)&addrlen))<0) 
-		{ 
-			perror("accept"); 
-			exit(EXIT_FAILURE); 
-		}
-		pid = fork();
-		if (pid==0) {
-			valread = read( new_socket , buffer, 1024); 
+    
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    
+    // Forcefully attaching socket to the port 6969
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( port ); 
+    
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address, 
+                                sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    }
+    
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                    (socklen_t*)&addrlen))<0) 
+        { 
+            perror("accept"); 
+            exit(EXIT_FAILURE); 
+        }
+        pid = fork();
+        if (pid==0) {
+            valread = read( new_socket , buffer, 1024); 
             char filename[100];
             char *fp = filename;
             int req_type = parse(buffer, fp);
@@ -241,6 +242,7 @@ int main(int argc, char const *argv[])
                     break;
                 case 6:
                     cgiHandler(fp, new_socket);
+            break;
                 default:
                     //unrecognized file type 
                     printf("unrecognized file type\n");
@@ -248,51 +250,51 @@ int main(int argc, char const *argv[])
 
             }
 
-			printf("%s\n",buffer );
+            printf("%s\n",buffer );
             close(new_socket);
             exit(0);
-			printf("Hello message sent\n"); 
-		} else {
-		    close(new_socket);	
-		}
-	}
+            printf("Hello message sent\n"); 
+        } else {
+            close(new_socket);  
+        }
+    }
 
-	//finished opening gif
-	
-	return 0; 
+    //finished opening gif
+    
+    return 0; 
 }
 
 void cgiHandler(char *filename, int new_socket) {
-	FILE *output;
-	FILE *script;
-	char buf[256];
-	char scriptBuf[256];
-	char response[10000];
-	char commandstring[500];
-	script = fopen(filename,"r");
-	fgets(scriptBuf,256,script);
-	if (strcmp(scriptBuf,"#!/bin/sh")==0) {
-		strcat(commandstring,"sh ");
-	} else {
-		strcat(commandstring,"perl ");
-	}
-	
-	strcat(commandstring,filename);
-	if ((output = popen(commandstring,"r"))==NULL) {
-		perror("Popen did not work");
-	} else {
-		char * header = "HTTP/1.1 200 OK\r\n";
-		while (fgets(buf,256,output)!=NULL) {
-			strcat(response,buf);
-			strcat(response,"\n");
-		}
-		pclose(output);
-		send(new_socket,header,strlen(header),0);
-		send(new_socket,response,strlen(response),0);
-	}
-	
+    FILE *output;
+    FILE *script;
+    char buf[256];
+    char scriptBuf[256];
+    char response[10000];
+    char commandstring[500];
+    script = fopen(filename,"r");
+    fgets(scriptBuf,256,script);
+    if (strcmp(scriptBuf,"#!/bin/sh")==0) {
+        strcat(commandstring,"sh ");
+    } else {
+        strcat(commandstring,"perl ");
+    }
+    
+    strcat(commandstring,filename);
+    if ((output = popen(commandstring,"r"))==NULL) {
+        perror("Popen did not work");
+    } else {
+        char * header = "HTTP/1.1 200 OK\r\n";
+        while (fgets(buf,256,output)!=NULL) {
+            strcat(response,buf);
+            strcat(response,"\n");
+        }
+        pclose(output);
+        send(new_socket,header,strlen(header),0);
+        send(new_socket,response,strlen(response),0);
+    }
+    
 
-}	
+}   
 
 
 
